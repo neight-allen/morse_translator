4a6f6e9227c3826389e1297cc2ab21009292f6b0
Nate Allen <nate@turing.io>
2017-08-29 10:11:51 -0600

    Iteration 0 working as described

--- test/translate_test.rb
+++ test/translate_test.rb
@@ -10,4 +10,19 @@ class TranslateTest < MiniTest::Test
     assert_instance_of Translate, translate
   end
 
+  def test_eng_to_morse_translates_one_word
+    translator = Translate.new
+
+    assert_equal ".....", translator.eng_to_morse("he")
+  end
+
+  def test_can_translate_english_string_to_morse_code
+    translator = Translate.new
+
+    actual = translator.eng_to_morse("hello world")
+    expected = "......-...-..--- .-----.-..-..-.."
+
+    assert_equal actual, expected
+  end
+
 end
